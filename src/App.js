42ccca2ce276d64ee5c1c1b44b72821012ac3e42
aycaaltin <aycaaltin0811@gmail.com>
2024-10-16 06:01:36 +0300

    odev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect} from 'react';
 function App() {
+  const products = [
+    { id: 1, name: 'Laptop', stock: 10,
+      category: 'electronics', price: 1000 },
+    { id: 2, name: 'Mouse', stock: 0, 
+      category: 'electronicks', price: 50 },
+    { id: 3, name: 'Apple', stock: 20,
+      category: 'grocery',  price: 1},
+    { id: 4, name: 'TV' , stock: 2, 
+      category: 'electronicks', price: 600},
+    { id: 5, name: 'Bread', stock: 15,
+      category: 'grocery', price: 2},
+    { id: 6, name: 'Headphones', stock: 5,
+      category: 'electronicks', price: 200},
+];
+
+const categoryProduct = {
+  electronicks: [],
+  grocery: [],
+  other: [],
+};
+const stockControl = (product)=> {  
+  console.log(`Ürün: ${product.name}`); 
+
+if (product.stock>0) {
+  console.log("Durum: InStock");
+} else {
+  console.log("Durum: Out of Stock");
+};
+}
+useEffect(() => {
+  products.forEach(product => {
+    switch (product.category) {
+      case "electronicks":
+        categoryProduct.electronicks.push(product);
+        break;
+      case "grocery":
+        categoryProduct.grocery.push(product);
+        break;
+      default:
+        categoryProduct.other.push(product);
+  
+  
+    }
+
+
+
+  
+  console.log(`Ürün adı: ${product.name}`);
+  console.log(`Ürün kategorisi: ${product.category}`);
+  console.log(`Ürün fiyatı: ${product.price}`);
+
+  stockControl(product);
+
+  });
+
+  
+  
+ 
+  //100 tl den fazla olan ürünleri listeleme
+  const expensiveProducts = products.filter(product => product.price > 100);
+  
+    expensiveProducts.forEach((product) => {
+      console.log(`Ürün adı: ${product.name}`);
+      console.log(`Ürün kategorisi: ${product.category}`);
+      console.log(`Ürün fiyatı: ${product.price}`);
+      // foreachle tüm ürünleri listeleme
+      products.forEach((product) => {
+        console.log(`Ürün adı: ${product.name}`);
+        console.log(`Ürün kategorisi: ${product.category}`);
+        console.log(`Ürün fiyatı: ${product.price}`);
+  
+      })
+  
+    console.log(`Ürün adı: ${product.name}`);
+    console.log(`Ürün kategorisi: ${product.category}`);
+    console.log(`Ürün fiyatı: ${product.price}`);
+  
+      
+  }
+  , [products]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
-}
+})}; 
+
+
 
 export default App;
